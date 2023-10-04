0c0538aa81bfa2f34378412f877018066c601b95
Arun-vj <arunvj1317@gmail.com>
2023-10-04 18:05:37 +0530

    sample band generator program

---
+++ band_generator.py
@@ -0,0 +1,4 @@
+print("welcome to band generator")
+city = input("what's name of the city you grew up in ?")
+name = input("what's your name ?")
+print("Your Band Name is {} {} band".format(name,city))
