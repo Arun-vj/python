8c006d93a4e4660d23776cdb952a6b74eb900c18
Arun <96170355+Arun-vj@users.noreply.github.com>
2023-10-04 18:25:25 +0530

    Delete band_generator.py

--- band_generator.py
+++
@@ -1,4 +0,0 @@
-print("welcome to band generator")
-city = input("what's name of the city you grew up in ?")
-name = input("what's your name ?")
-print("Your Band Name is {} {} band".format(name,city))
